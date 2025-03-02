60c6a3166289066f23edd5a37677b4cb53d9ee2d
James Tocknell <aragilar@gmail.com>
2025-03-02 13:46:37 +1100

    git-annex in aragilar@callisto:~/Projects/Uni/PhD/PPDSolutions

--- test_shock.py
+++ test_shock.py
@@ -2,11 +2,13 @@ from disc_solver.file_format import registries
 from disc_solver.solve.shock import compute_shock
 from h5preserve import open as h5open
 
-f = h5open("../PPDSolutions/sonic_library/new_miller_runs/complete_csvs/run_3/stepped_runs/2024-01-09T07:52:19.526761+00:00ForkPoolWorker-15.hdf5", registries, mode='r')
-soln = f["run"].final_solution
+f = h5open("sonic_library/new_miller_runs/complete_csvs/run_6/stepped_runs/2024-01-24T04:10:52.557273+11:00ForkPoolWorker-1.hdf5", registries, mode='r')
+#soln = f["run"].final_solution
+soln = f["run"].solutions["29"]
 solns = []
 for i, shock_soln in enumerate(compute_shock(
-    soln, min_angle=58, max_v_θ_pre=10, min_v_θ_pre=1.5
+    soln, min_angle=58, max_v_θ_pre=10, min_v_θ_pre=1.5,
+    plot_prefix="shock_test_plots/fixed_E_r_file_1",
 )):
     print("Solution", i, "shown")
     #solns.append(shock_soln)
